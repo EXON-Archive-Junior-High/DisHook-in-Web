ee097060c9c860bc7a02fbd9afaec230251fab17
시혁 박 <exon.indiv@gmail.com>
2020-09-16 16:45:59 +0900

    add avatar, name

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const webhookContent = document.querySelector("#webhook-content")
 
-function sendWebhook(url, content){
+function sendWebhook(url, content, name, avatar_url){
     
     fetch(url, {
-     "method":"POST",
-     "headers": {"Content-Type" : "application/json"},
-     "body": JSON.stringify({
-        "content":content
-      })
+        "method":"POST",
+        "headers": {"Content-Type" : "application/json"},
+        "body": JSON.stringify({
+            "content":content,
+            "username":name,
+            "avatar_url":avatar_url
+        })
     })
     .then(res=> console.log(res))
     .catch(err => console.error(err))
